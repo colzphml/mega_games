cd16dd92e1dc5fd07c6b1eae1a6ca3f25e2506d1
Сергей Порубов <colz@MacBook-Pro--Colz.local>
2024-05-18 23:05:53 +0300

    colz: changed users

--- internal/sourceSM/discord/discord.go
+++ internal/sourceSM/discord/discord.go
@@ -213,7 +213,17 @@ func (c *Client) buildScheduleText(title string, games []model.Game) string {
 	for _, game := range games {
 		home := c.Teams.Teams[game.Home]
 		away := c.Teams.Teams[game.Away]
-		gameInfo := fmt.Sprintf("\n[%s](%s) @ [%s](%s)", away.ShortName, "t.me/"+away.Player[1:], home.ShortName, "t.me/"+home.Player[1:])
+		var gameInfo string
+		if away.Player == "CPU" && home.Player == "CPU" {
+			gameInfo = fmt.Sprintf("\n%s(CPU) @ %s(CPU)", away.ShortName, home.ShortName)
+		} else if away.Player == "CPU" {
+			gameInfo = fmt.Sprintf("\n%s(CPU) @ [%s](%s)", away.ShortName, home.ShortName, "t.me/"+home.Player[1:])
+		} else if home.Player == "CPU" {
+			gameInfo = fmt.Sprintf("\n[%s](%s) @ %s(CPU)", away.ShortName, "t.me/"+away.Player[1:], home.ShortName)
+		} else {
+			gameInfo = fmt.Sprintf("\n[%s](%s) @ [%s](%s)", away.ShortName, "t.me/"+away.Player[1:], home.ShortName, "t.me/"+home.Player[1:])
+		}
+
 		textBuilder.WriteString(gameInfo)
 	}
 
